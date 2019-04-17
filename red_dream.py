acb0e504bdcca26ea568fa39f650f1f2677c805c
medal <webformedal@gmail.com>
2019-04-17 18:03:16 +0300

    Построение интерфейса.

--- red_dream.py
+++ red_dream.py
@@ -1,1 +1,43 @@
 # medal red dream
+# импортируем библиотеки
+from flask import Flask, request
+import logging
+import requests
+
+import json
+
+app = Flask(__name__)
+
+logging.basicConfig(level=logging.INFO)
+sessionStorage = {}
+
+
+@app.route('/post', methods=['POST'])
+def main():
+    logging.info('Request: %r', request.json)
+
+    # Начинаем формировать ответ, согласно документации
+    # мы собираем словарь, который потом при помощи библиотеки json преобразуем в JSON и отдадим Алисе
+    response = {
+        'session': request.json['session'],
+        'version': request.json['version'],
+        'response': {
+            'end_session': False
+        }
+    }
+
+    handle_dialog(request.json, response)
+
+    logging.info('Response: %r', request.json)
+
+    return json.dumps(response)
+
+
+def handle_dialog(req, res):
+    user_id = req['session']['user_id']
+    query = req['request']['original_utterance']
+    res['response']['text'] = ""
+
+
+if __name__ == '__main__':
+    app.run()
