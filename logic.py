290a1def046c95ec0bf1374c00eb7293735e1848
saichuckp <saichuckp@gmail.com>
2019-04-17 18:44:28 +0300

    Продолжаю создавать юзера

--- logic.py
+++ logic.py
@@ -1,7 +1,14 @@
+import json
+
+
 class User:
 
     def __init__(self, id):
         self.move = 0
         self.id = id
-
-        self.period
+        self.period = ""
+        self.government = 0
+        self.economy = 0
+        self.military = 0
+        self.control = 0
+        self.communism = 0
