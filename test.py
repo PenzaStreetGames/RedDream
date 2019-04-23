56e61c58cf02b023cc23e60bfa1fdc6f2b29398a
Gamba232 <parassitn@mail.ru>
2019-04-24 00:13:41 +0300

    Дорабтка json и тесты

--- test.py
+++ test.py
@@ -1,60 +1,104 @@
 import json
 
 
-def is_dict(data):
-    return type(data) == dict
+class TestError(Exception):
+    pass
 
 
-def cat_questions(data):
-    """ Хватает ли вопросов в каждой категории / Есть ли вопросы к несуществующим категориям"""
-    counts = {}
-    real = {}
-    for el in data["periods"]:
-        if is_dict(el):
-            counts[el["name"]] = el["length"]
-    for el in data["questions"]:
-        real[el["period"]] = real.get(el["period"], 0) + 1
-    return counts == real
+def questions_number(data):
+    """ Хватает ли вопросов в каждой категории"""
+    questions = data["questions"]
+    categories = data["periods"]
+    for category in categories:
+        category_questions = list(
+            filter(lambda question: question["period"] == category["name"],
+                   data["questions"]))
+        if len(category_questions) < category["length"]:
+            raise TestError(f"В категории  {category['name']} нехватает "
+                            f"вопросов: {len(category_questions)} вместо "
+                            f"{category['length']}")
+    return "Все категории имеют достаточно вопросов"
+
+
+def questions_category(data):
+    """Есть ли вопросы к несуществующим категориям"""
+    categories = list(map(lambda category: category["name"],
+                          data["periods"]))
+    for question in data["questions"]:
+        period = question["period"]
+        if period not in categories:
+            raise TestError(f"Вопрос {question['text'][:30]}... относится к"
+                            f"несуществующей категории {period}")
+    return "Все вопросы относятся к существующим категориям"
 
 
 def is_period_jumps(data):
     """ Есть ли переходы между всеми периодами """
-    pass
+    categories = list(map(lambda category: category["name"],
+                          data["periods"]))
+    category_jumps = ["стартовый"]
+    for i in range(len(categories) - 1):
+        category_jumps += [f"{categories[i]}-{categories[i + 1]}"]
+    jumps = list(data["jumps"].keys())
+    if category_jumps != jumps:
+        raise TestError(f"Переходы различаются \n Нужны:   {category_jumps} \n "
+                        f"Имеются: {jumps}")
+    return "Имеются все необходимые переходы"
 
 
 def is_null_question_variants(data):
     """ Есть ли вопросы без вариантов ответа """
-    counts = {}
-    real = {}
-    for el in data["questions"]:
-        if not len(el["answers"]):
-            return False
-    return True
+    for question in data["questions"]:
+        if len(question["answers"]) == 0:
+            raise TestError(f"У вопроса {question['text'][:30]}... нет "
+                            f"вариантов ответа")
+    return "Все вопросы имеют варианты ответа"
+
+
+def is_none_variant_causes(data):
+    """  Есть ли вопросы без указания последствий """
+    effects = ["government", "economy", "military", "control",
+               "communism"]
+    for question in data['questions']:
+        for answer in question["answers"]:
+            if answer.get("effects", None) is None:
+                raise TestError(
+                    f"Ответ {answer['text'][:30]} вопроса "
+                    f"{question['text'][:30]}"
+                    f"Не имеет последствий")
+            for effect in effects:
+                if answer["effects"].get(effect) is None:
+                    raise TestError(
+                        f"Ответ {answer['text'][:30]} вопроса "
+                        f"{question['text']}"
+                        f"Не имеет поля {effect}")
+
+    return "Все ответы имеют последствия"
 
 
 def is_null_variant_causes(data):
-    """  Есть ли вопросы без указания последствий """
-    counts = {}
-    real = {}
-    for question in data["questions"]:
-        for el in question["answers"]:
-            if not el["cause"]:
-                return False
-    return True
+    """ Есть ли вопросы с нудевыми последствиями """
+    for question in data['questions']:
+        for answer in question["answers"]:
+            effects = answer["effects"].values()
+            if all(list(map(lambda effect: effect == 0, effects))):
+                raise TestError(
+                    f"Ответ {answer['text'][:30]} вопроса "
+                    f"{question['text'][:30]}... "
+                    f"имеет нулевые последствия")
+    return "Все ответы имеют ненулевые последствия"
 
 
-
-with open("quest.json", "r", encoding="utf8") as file:
-    quest = json.loads(file.read())
-
-
-def make_questions_list(data):
-    questions_list = []
-    for question in data["questions"]:
-        questions_list.append(question)
-
-    questions_list.sort(key=lambda el: el["date"])
-
-    return questions_list
-
-print(make_questions_list(quest))
+if __name__ == '__main__':
+    with open("quest.json", "r", encoding="utf8") as file:
+        quest = json.loads(file.read())
+    try:
+        print(questions_number(quest))
+        print(questions_category(quest))
+        print(is_period_jumps(quest))
+        print(is_null_question_variants(quest))
+        print(is_none_variant_causes(quest))
+        print(is_null_variant_causes(quest))
+        print("Все тесты пройдены успешно")
+    except TestError as error:
+        print(error)
